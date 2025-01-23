d2a0b6cf8ed594f97ab1f7d28c266730e01a5cfa
jasantiaguel <jsantiaguel2@my.bcit.ca>
2025-01-23 15:19:23 -0800

    editing scrolling text content

--- src/components/SlidingText.js
+++ src/components/SlidingText.js
@@ -10,11 +10,21 @@ export default function SlidingText() {
   const firstText = useRef(null);
   const secondText = useRef(null);
   const slider = useRef(null);
+  const sliderContainer = useRef(null);
   let xPercent = 1;
   let direction = -1;
 
   useEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
+    gsap.fromTo(
+      sliderContainer.current,
+      { opacity: 0 },
+      { opacity: 1, 
+        duration: 2,
+        delay: 3.6,
+        ease: "power2.out" }
+    );
+
     requestAnimationFrame(animation);
 
     gsap.to(slider.current, {
@@ -24,10 +34,10 @@ export default function SlidingText() {
         end: window.innerHeight,
         scrub: true,
         onUpdate: (e) => {
-          direction = e.direction * -1;
+          direction = e.direction * 1;
         },
       },
-      y: "+=200px",
+      y: "-=200px",
     });
   }, []);
 
@@ -45,22 +55,22 @@ export default function SlidingText() {
   };
 
   return (
-    <div className={styles.sliderContainer}>
+    <div ref={sliderContainer} className={styles.sliderContainer}>
       <div ref={slider} className={styles.slider}>
         <p ref={firstText}>
-          PORTFOLIO 2025^
+          PORTFOLIO 2025
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
           JONATHAN ANDREW
-          SANTIAGUEL Y/1997
+          SANTIAGUEL Y.1997
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
           INNOVATION · EFFICIENCY · CREATIVITY
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
         </p>
         <p ref={secondText}>
-          PORTFOLIO 2025^
+          PORTFOLIO 2025
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
           JONATHAN ANDREW
-          SANTIAGUEL Y/1997
+          SANTIAGUEL Y.1997
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
           INNOVATION · EFFICIENCY · CREATIVITY
           &nbsp;&nbsp;&nbsp;//&nbsp;&nbsp;&nbsp;
