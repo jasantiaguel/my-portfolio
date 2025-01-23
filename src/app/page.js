ae08ca08c9bef453d49ae0a0a7555bbbc8b2b23c
jasantiaguel <jsantiaguel2@my.bcit.ca>
2025-01-23 15:26:33 -0800

    installing and adding locomotive scroll to page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,21 @@
+"use client";
+
 // Components
+import { useEffect } from "react";
 import InteractiveTitle from "@/components/InteractiveTitle";
 import SlidingText from "@/components/SlidingText";
 
 export default function Home() {
+
+  useEffect(() => {
+    (
+      async () => {
+        const LocomotiveScroll = (await import("locomotive-scroll")).default;
+        const locomotiveScroll = new LocomotiveScroll();
+      }
+    )()
+  }, []);
+
   return (
     <main className="main">
               <SlidingText />
